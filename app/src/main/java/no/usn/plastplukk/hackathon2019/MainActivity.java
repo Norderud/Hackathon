b005e392d191f64034b7ddf5bc6fb28e0116382e
Sannsynlighet <asmundno@live.no>
2019-04-05 20:42:27 +0200

    sdf

--- app/src/main/java/no/usn/plastplukk/hackathon2019/MainActivity.java
+++ app/src/main/java/no/usn/plastplukk/hackathon2019/MainActivity.java
@@ -1,10 +1,9 @@
 package no.usn.plastplukk.hackathon2019;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
