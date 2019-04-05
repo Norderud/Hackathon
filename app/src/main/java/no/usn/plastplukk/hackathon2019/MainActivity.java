ee77ee2e673a3d4ca42040f2b9e0c54c3faf66e9
Danay <dnelvik@gmail.com>
2019-04-05 22:37:04 +0200

    login

--- app/src/main/java/no/usn/plastplukk/hackathon2019/MainActivity.java
+++ app/src/main/java/no/usn/plastplukk/hackathon2019/MainActivity.java
@@ -1,13 +1,26 @@
 package no.usn.plastplukk.hackathon2019;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    SharedPreferences prefs;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        prefs = getSharedPreferences("MyPrefsName", MODE_PRIVATE);
+
+        if (prefs.getString("Username", "").length() == 0){
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivity(intent);
+        }
+
     }
 }
