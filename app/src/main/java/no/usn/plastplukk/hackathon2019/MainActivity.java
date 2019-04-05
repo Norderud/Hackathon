e782a3af9a32185e82c898c607df58a034a82826
Sannsynlighet <asmundno@live.no>
2019-04-05 21:15:10 +0200

    fikset floating action button

--- app/src/main/java/no/usn/plastplukk/hackathon2019/MainActivity.java
+++ app/src/main/java/no/usn/plastplukk/hackathon2019/MainActivity.java
@@ -3,13 +3,17 @@ package no.usn.plastplukk.hackathon2019;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.support.design.widget.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
+    FloatingActionButton fab1, fab2, fab3;
+    boolean isFABOpen;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        isFABOpen = false;
+        FloatingActionButton fab = findViewById(R.id.fab);
         fab1 = (FloatingActionButton) findViewById(R.id.fab1);
         fab2 = (FloatingActionButton) findViewById(R.id.fab2);
         fab3 = (FloatingActionButton) findViewById(R.id.fab3);
@@ -23,5 +27,18 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+    private void showFABMenu(){
+        isFABOpen=true;
+        fab1.animate().translationY(-55);
+        fab2.animate().translationY(-105);
+        fab3.animate().translationY(-155);
+    }
+
+    private void closeFABMenu(){
+        isFABOpen=false;
+        fab1.animate().translationY(0);
+        fab2.animate().translationY(0);
+        fab3.animate().translationY(0);
     }
 }
