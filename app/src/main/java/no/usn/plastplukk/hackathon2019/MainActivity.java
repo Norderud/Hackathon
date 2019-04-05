7ece8571171f7c344e60e873041a21444a97cc71
Sannsynlighet <asmundno@live.no>
2019-04-05 21:03:23 +0200

    sadfsad

--- app/src/main/java/no/usn/plastplukk/hackathon2019/MainActivity.java
+++ app/src/main/java/no/usn/plastplukk/hackathon2019/MainActivity.java
@@ -2,11 +2,26 @@ package no.usn.plastplukk.hackathon2019;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab1 = (FloatingActionButton) findViewById(R.id.fab1);
+        fab2 = (FloatingActionButton) findViewById(R.id.fab2);
+        fab3 = (FloatingActionButton) findViewById(R.id.fab3);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(!isFABOpen){
+                    showFABMenu();
+                }else{
+                    closeFABMenu();
+                }
+            }
+        });
     }
 }
