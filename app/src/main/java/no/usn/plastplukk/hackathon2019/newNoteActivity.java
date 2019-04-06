58af5a5e28859d76ca759ed09b8473d9de0c506d
Sannsynlighet <asmundno@live.no>
2019-04-06 12:55:31 +0200

    asd

--- app/src/main/java/no/usn/plastplukk/hackathon2019/newNoteActivity.java
+++ app/src/main/java/no/usn/plastplukk/hackathon2019/newNoteActivity.java
@@ -1,39 +1,68 @@
 package no.usn.plastplukk.hackathon2019;
 
-import android.graphics.Typeface;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.text.SpannableString;
-import android.text.SpannableStringBuilder;
-import android.util.Log;
-import android.view.KeyEvent;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class newNoteActivity extends AppCompatActivity {
-    TextView titleView, editTextView;
-    boolean isBold;
-    SpannableStringBuilder spanString;
+import net.dankito.richtexteditor.android.RichTextEditor;
 
+public class NewNoteActivity extends AppCompatActivity {
+
+    private RichTextEditor editor;
+    private boolean isBold, isItalic;
+    private Button boldButton, italicButton;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_note);
-        titleView = findViewById(R.id.titleET);
-        editTextView = findViewById(R.id.text);
-        spanString = new SpannableStringBuilder();
-        editTextView.setOnKeyListener(new View.OnKeyListener() {
-            @Override
-            public boolean onKey(View v, int keyCode, KeyEvent event) {
-                editTextView.append("dåse");
-                return true;
-            }
-        });
 
+        boldButton = findViewById(R.id.boldButton);
+        italicButton = findViewById(R.id.italicButton);
+        editor = findViewById(R.id.editor);
+        editor.setEditorFontSize(20);
+        editor.setPadding((int) (4 * getResources().getDisplayMetrics().density));
+        editor.focusEditorAndShowKeyboardDelayed();
+    }
+
+    public void sjekk(View view) {
+        Toast.makeText(this, editor.getHtml(),Toast.LENGTH_LONG).show();
     }
 
     public void boldText(View view) {
-        isBold = !isBold;
+        if(!isBold){
+            boldButton.setBackgroundColor(getResources().getColor(R.color.colorGreen));
+            editor.setBold();
+            isBold = true;
+        } else {
+            boldButton.setBackgroundColor(getResources().getColor(R.color.colorGray));
+            editor.setBold();
+            isBold = false;
+        }
+    }
+    public void addImage(){
+        StringBuilder html = new StringBuilder();
+        html.append(editor.getHtml());
+        html.append("<img src=\"https://itfag.usn.no/grupper/v19gr2/plast/itfag/uploadedFiles/JPEG_20190320_150316_1553090596743_.jpg\" width=\"70%\">");
+        editor.setHtml(html.toString());
+    }
+
+    public void italicText(View view) {
+        if(!isItalic){
+            italicButton.setBackgroundColor(getResources().getColor(R.color.colorGreen));
+            editor.setItalic();
+            isItalic = true;
+        } else {
+            italicButton.setBackgroundColor(getResources().getColor(R.color.colorGray));
+            editor.setItalic();
+            isItalic = false;
+        }
+    }
+
+    public void addPhoto(View view) {
 
     }
 }
+
